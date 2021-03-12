fadc32660314ef381fb6ff3d5eb063372ffe57e8
Brian Garcia <garciab0355@my.uwstout.edu>
2021-03-11 22:36:48 -0600

    SaveToCSV is finished.  Need to test for errors.

--- ComputerInterface/Function.cpp
+++ ComputerInterface/Function.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <ctime>
 
 using namespace std;
 
@@ -23,5 +24,23 @@ void Event::DataIn() {
 }
 
 void Event::SaveToCSV(vector<string> data) {
+	ofstream outdata;
+	string filename;
+	time_t rawtime;
+	struct tm* timeinfo;
+	char buffer[80];
+
+	time(&rawtime);
+	timeinfo = localtime(&rawtime);
+
+	strftime(buffer, sizeof(buffer), "%d-%m-%Y %H.%M.%S", timeinfo);
+	string str(buffer);
+	filename = "Solar Data " + str + ".csv";
+	outdata.open("C:\\Users\\kitsu\\Desktop\\" + filename);
+	for (int i = 0; i < data.size(); i++) {
+		cout << data[i] << endl;
+	}
+	outdata.close();
+
 
 }
