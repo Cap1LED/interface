b24f2aac349889f7df411d2765d9ddf08c5b82eb
Jack Hayes <hayesj6816@my.uwstout.edu>
2021-04-14 18:42:25 -0500

    Proof of concept Rust web API backend
    This still requires integration with the I2C code

---
+++ src/main.rs
@@ -0,0 +1,76 @@
+use actix_web::{
+    middleware, web, App, HttpResponse, HttpServer,
+};
+use actix_files as fs;
+use serde::{Deserialize, Serialize};
+use serde_with::{serde_as, DisplayFromStr};
+
+#[derive(Serialize, Deserialize)]
+struct UCData {
+    boards: Vec<u8>,
+    duty_cycle: Vec<Vec<f32>>,
+    temperature: Vec<f32>,
+    error: bool,
+}
+
+#[serde_as]
+#[derive(Serialize, Deserialize)]
+struct UCCommand{
+    #[serde_as(as = "DisplayFromStr")]
+    board: u8,
+    #[serde_as(as = "Vec<Vec<DisplayFromStr>")]
+    duty_cycle: Vec<f32>,
+}
+
+/// This function detects the microcontrollers via I2C and stores the addresses they responded on in shared state
+async fn board_detect() {
+    // TODO: Add I2C Code
+    
+}
+
+/// This function reads the data from each board from I2C.
+/// The data takes the form of
+async fn get_data() -> HttpResponse {
+    // TODO: Read the shared state we read with board_detect()
+    
+    // TODO: Read the I2C data about duty cycle
+    
+    // Dummy Response
+    HttpResponse::Ok().json(&UCData{
+        boards: vec![0x10],
+        duty_cycle: vec![vec![10.0, 20.0, 30.0, 40.0]],
+        temperature: vec![27.0],
+        error: false,
+    })
+}
+
+async fn set_duty_cycle(item: web::Json<UCCommand>) -> HttpResponse {
+    HttpResponse::Ok().json(&UCData{
+        boards: vec![0x10],
+        duty_cycle: vec![vec![10.0, 20.0, 30.0, 40.0]],
+        temperature: vec![27.0],
+        error: false,
+    })
+}
+
+
+/// This function creates the HTTP Server, defines the API and enables the logger
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
+    //TODO: Board detect
+    HttpServer::new(|| {
+        App::new()
+            // enable logger
+            .wrap(middleware::Logger::default())
+            // create API routes
+            .service(web::resource("/dutyCycle").route(web::post().to(set_duty_cycle)))
+            .service(web::resource("/data").route(web::get().to(get_data)))
+            //  serve static HTML
+            .service(fs::Files::new("/", "./static").show_files_listing())
+    })
+    .bind("0.0.0.0:8080")?
+    .run()
+    .await
+}
---
+++ static/helper.js
@@ -0,0 +1,92 @@
+var dataset = []; // To store all data
+
+
+//  This function updates the text box so we have
+//  not just a visual representation but also a number
+function updateText(){
+
+}
+
+// This function exports the current data to .csv
+// and provides the user with a save dialog
+function exportData() {
+    let csvContent = "data:text/csv;charset=utf-8," 
+    + dataset.map(e => e.join(",")).join("\n");
+    var encodedUri = encodeURI(csvContent);
+    window.open(encodedUri);
+}
+
+// This function clears the current data
+function clearData() {
+    location.reload()
+}
+
+// This function creates a POST to the API
+// that then communicates with the a microcontroller
+// on the selected address via I2C (20 bytes, 4xF16)
+function dutyCycle() {
+    data = {
+        "board" : "1",
+        "duty_cycle" : [0,0,0,0,0],
+    }
+    fetch("/dutyCycle", {
+        method : 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+          body: JSON.stringify(data)
+    });
+}
+
+// This function creates a GET to the API
+// that reads the current duty cycle and
+// temperature via I2C for each board
+// that was detected on startup
+async function data(){
+    await fetch("/data")
+    .then(response => response.json())
+    .then(data =>  {
+        //Append the current data to the list of data points
+        dataset.push([data.boards, data.duty_cycle, data.temperature]);
+    });
+}
+
+
+
+// Copy-pasted from https://canvasjs.com/html5-javascript-dynamic-chart/
+window.onload = function () {
+    var dps = []; // Temperature datapoints
+    var xVal = 0; // index
+    var chart = new CanvasJS.Chart("chartContainer", {
+        title :{
+            text: "Temperature"
+        },
+        data: [{
+            type: "line",
+            dataPoints: dps
+        }]
+    });
+
+    var updateInterval = 1000;
+    var dataLength = 15; // number of dataPoints visible at any point
+    
+    var updateChart = async function () {
+        // Poll API for current data
+        await data();
+        // Fill dps with data from dataset
+        dps.push({
+                x: xVal,
+                y: dataset[xVal][2][0],
+                });
+        xVal++;
+        if (dps.length > dataLength) {
+            dps.shift();
+        }
+    
+        chart.render();
+    };
+    
+    updateChart();
+    setInterval(function(){updateChart()}, updateInterval);
+};
+
